611ccac6159b11267adc1c49f29dbd38dfe60fe2
sparkeplug <prabhu.rk90@gmail.com>
2019-08-23 23:15:08 +0530

    Fixed bug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import './carousel.scss';
 import Carousel from './carousel';
 
 // Write Javascript code!
-let carousels = document.querySelectorAll('.carousel-wrapper');
+let carousels = document.querySelectorAll('.gt-carousel');
 let carouselsInits = [];
 carousels.forEach(carousel => {
   carouselsInits.push(new Carousel(carousel,1));
